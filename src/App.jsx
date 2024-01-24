9db4af8a5d7efbd684a81ab3de93a0973f94aef3
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-01-24 13:20:42 +0100

    video added using reactplayer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import { useState } from 'react'
 import './App.css'
 import ReactPlayer from "react-player"
 
+
+
 function App() {
   const [count, setCount] = useState(0)
+  const vidURL ="https://www.youtube.com/shorts/PhezGvPwVnA"
 
   return (
     <>
+    <div>
+      <h1>A mandate That Divinity May Be Express in Mortality Through The Ministry Of worship  </h1>
+
+      <ReactPlayer
+      url={vidURL}
+      playing={false}
+      volume = {0.5}
+      />
+
+    </div>
   
     </>
   )
